caad6e905378f43b633adc5a5e70c95a5de21ab3
Realm4M3 <tavionb2003@gmail.com>
2026-03-31 00:54:54 -0400

    Update CardItem.kt

--- CardItem.kt
+++ CardItem.kt
@@ -5,5 +5,7 @@ data class CardItem(
     val imageRes: Int? = null,
     val imagePath: String? = null,
     var isFlipped: Boolean = false,
-    var isMatched: Boolean = false
+    var isMatched: Boolean = false,
+    var hitCount: Int = 0,
+    var isBroken: Boolean = false
 )
